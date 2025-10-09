3abf16860e990e633dceb3a860fc8f2fef091e12
Dhirendra Singh <dhirendra2834@gmail.com>
2025-10-09 09:01:18 +0530

    add service layer

--- src/main/java/com/example/demo/service/SubjectService.java
+++ src/main/java/com/example/demo/service/SubjectService.java
@@ -1,9 +1,41 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Course;
+import com.example.demo.model.Subject;
+import com.example.demo.model.User;
+import com.example.demo.repository.SubjectRepository;
 
 @Service
 public class SubjectService {
-
+	
+	@Autowired
+	SubjectRepository subjectRepository;
+	
+	@Autowired
+	CourseService courseService;
+	
+	@Autowired 
+	UserService userService;
+	
+	
 	// Create the Subject 
+	public String createSubject(Subject subject) {
+		User user=subject.getCreatedBy();
+		Optional<User> userData=userService.getById(user.getId());
+		
+		Course course=subject.getCourseId();
+		Optional<Course> courseData=courseService.getCourseByCourseId(course.getId());
+	
+		if(user.isPresent() && course.isPresent()) {
+			
+		}
+		return null;
+		
+	}
+	
 }
