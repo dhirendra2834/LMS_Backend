c440e0ca77900ab1c4569ecd04926bfef4d5d29b
Dhirendra Singh <dhirendra2834@gmail.com>
2025-10-16 19:35:43 +0530

    Changes

--- src/main/java/com/example/demo/controller/ChapterController.java
+++ src/main/java/com/example/demo/controller/ChapterController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
-public class ChapterController {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.demo.service.ChapterService;
+
+public class ChapterController {
+	
+	@Autowired
+	ChapterService chapterService;
+
+	// Create Chapter 
 }
